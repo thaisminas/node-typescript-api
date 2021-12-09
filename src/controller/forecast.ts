7869ae34c56fceed9854fc21b2c0ac6a517c9db3
thaisminas <thaisminasds@gmail.com>
2021-12-09 18:27:41 -0300

    removing node_modules

---
+++ src/controller/forecast.ts
@@ -0,0 +1,11 @@
+import { controller, Get } from '@overnightjs/core';
+
+@controller('forecast')
+export class ForecastController {
+
+    @Get('')
+    public getForecastLoggerdUser(): void {
+        
+
+    }
+}
